d07008914a6fe7a5e5895bd137d6ee7066c71a5f
Phy <git@phy25.com>
2019-08-26 01:07:46 -0400

    Remove cancelled_at filter for regular bookings

--- bookings/admin.py
+++ bookings/admin.py
@@ -8,7 +8,7 @@ from .models import Booking, CancelledBooking
 class BookingAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at', 'cancelled_at', )
     list_display = ('email', 'event_type_id', 'spot_start', 'booked_at', 'approval_status')
-    list_filter = ('approval_status', 'cancelled_at', 'booked_at', 'spot_start', 'event_type_id')
+    list_filter = ('approval_status', 'booked_at', 'spot_start', 'event_type_id')
 
 
 @admin.register(CancelledBooking)
