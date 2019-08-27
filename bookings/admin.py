9c077245ca13f5455ab7e2cd54c912fddc94f28f
Phy <git@phy25.com>
2019-08-26 22:34:39 -0400

    Add approval actions

--- bookings/admin.py
+++ bookings/admin.py
@@ -7,8 +7,37 @@ from .models import Booking, CancelledBooking
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at', 'cancelled_at', )
-    list_display = ('email', 'event_type_id', 'spot_start', 'booked_at', 'approval_status')
+    list_display = ('email', 'event_type_id', 'spot_start', 'booked_at', 'approval_status', 'approval_protected')
     list_filter = ('approval_status', 'booked_at', 'spot_start', 'event_type_id')
+
+    def approve_and_protect(self, request, queryset):
+        try:
+            changed = queryset.update(approval_status=Booking.APPROVAL_STATUS_APPROVED, approval_protected=True)
+            self.message_user(request, "Approved and protected "+str(changed)+" rows.")
+        except Exception as e:
+            self.message_user(request, str(e), messages.ERROR)
+    approve_and_protect.short_description = "Approve and protect"
+    approve_and_protect.allowed_permissions = ('change',)
+
+    def decline_and_protect(self, request, queryset):
+        try:
+            changed = queryset.update(approval_status=Booking.APPROVAL_STATUS_DECLINED, approval_protected=True)
+            self.message_user(request, "Declined and protected "+str(changed)+" rows.")
+        except Exception as e:
+            self.message_user(request, str(e), messages.ERROR)
+    decline_and_protect.short_description = "Decline and protect"
+    decline_and_protect.allowed_permissions = ('change',)
+
+    def reset_approval(self, request, queryset):
+        try:
+            changed = queryset.update(approval_status=Booking.APPROVAL_STATUS_NEW, approval_protected=False)
+            self.message_user(request, "Reseted "+str(changed)+" rows.")
+        except Exception as e:
+            self.message_user(request, str(e), messages.ERROR)
+    reset_approval.short_description = "Reset approval"
+    reset_approval.allowed_permissions = ('change',)
+
+    actions = [approve_and_protect, decline_and_protect, reset_approval]
 
 
 @admin.register(CancelledBooking)
