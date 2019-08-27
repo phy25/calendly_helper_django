6a1b75d0c7d46593d5fc77f4b02ddf6dbf608eea
Phy <git@phy25.com>
2019-08-27 19:09:56 -0400

    Add favicon

--- calendly_helper/urls.py
+++ calendly_helper/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 from constance import config
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 urlpatterns = [
     path('', include('webhook_calendly.frontend_urls')),
     path('', include('bookings.urls')),
     path('calendly/', include('webhook_calendly.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'), permanent=True))
 ]
 
 
