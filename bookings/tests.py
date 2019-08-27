0675fbaff9bdde4b652ea4cee73a30e9edf97252
Phy <git@phy25.com>
2019-08-27 19:50:29 -0400

    Add basic tests

--- bookings/tests.py
+++ bookings/tests.py
@@ -1,3 +1,18 @@
+import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Booking, CancelledBooking
+
+
+class CancelledBookingTests(TestCase):
+    def test_deleting_booking(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        b = Booking(spot_start=1, spot_end=2)
+        #b.delete()
+
+    def test_is_proxy(self):
+        self.assertEqual(CancelledBooking._meta.proxy, True)
