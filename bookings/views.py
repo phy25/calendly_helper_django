cae64f528c5366314217c1da718b7eced50eb104
Phy <git@phy25.com>
2019-09-01 00:49:43 -0400

    Fix missing context in bookings.views

--- bookings/views.py
+++ bookings/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def student_reports(request):
+    context = {}
     return render(request, 'bookings/base.html', context)
