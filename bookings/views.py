2c6e53cc0aedb9d2390e418c6b6f9e4aaa99faf5
Phy <git@phy25.com>
2019-08-31 23:27:14 -0400

    Remove stub in bookings.view

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,2 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def student_reports(request):
-    context = {'invalid_bookings_count': 1}
-    return render(request, 'bookings/student_reports.html', context)
