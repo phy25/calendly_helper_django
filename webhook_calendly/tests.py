4bb9a8a1f93998a0aaf3c7eb80c7bdccfb8bbcc9
Phy <git@phy25.com>
2019-09-01 17:12:32 -0400

    Added test_execute_approval_fake

--- webhook_calendly/tests.py
+++ webhook_calendly/tests.py
@@ -215,6 +215,98 @@ class ApprovalTests(TestCase):
         self.assertEqual(len(bookings_declined), 2)
         self.assertEqual(set(bookings_declined), set([bc2.booking, bc3.booking]))
 
+    def test_get_approval_executor_declined(self):
+        ag = ApprovalGroup.objects.get(name="Group 1")
+        ag.approval_type = ApprovalGroup.APPROVAL_TYPE_DECLINED
+
+        Invitee.objects.create(email="aa@localhost", group=ag)
+
+        bc2 = BookingCalendlyData.objects.create(
+            calendly_uuid="5",
+            booking=Booking.objects.create(
+                email="a@localhost",
+                event_type_id="2",
+                spot_start="2019-01-01 15:40:00-0400",
+                spot_end="2019-01-01 15:50:00-0400",
+                approval_status=Booking.APPROVAL_STATUS_NEW,
+            ),
+        )
+
+        bc3 = BookingCalendlyData.objects.create(
+            calendly_uuid="6",
+            booking=Booking.objects.create(
+                email="aa@localhost",
+                event_type_id="2",
+                spot_start="2019-01-01 10:00:00-0400",
+                spot_end="2019-01-01 10:10:00-0400",
+                approval_status=Booking.APPROVAL_STATUS_NEW,
+            ),
+        )
+
+        bookings_approved, bookings_declined = ag.get_approval_executor("2")
+        self.assertEqual(len(bookings_approved), 0)
+        self.assertEqual(len(bookings_declined), 3)
+
+    def test_execute_approval_fake(self):
+        "Make sure fake does not change anything and returns the correct result"
+        ag = ApprovalGroup.objects.get(name="Group 1")
+        Invitee.objects.create(email="aa@localhost", group=ag)
+
+        bc2 = BookingCalendlyData.objects.create(
+            calendly_uuid="5",
+            booking=Booking.objects.create(
+                email="a@localhost",
+                event_type_id="2",
+                spot_start="2019-01-01 15:40:00-0400",
+                spot_end="2019-01-01 15:50:00-0400",
+                approval_status=Booking.APPROVAL_STATUS_NEW,
+            ),
+        )
+
+        bc3 = BookingCalendlyData.objects.create(
+            calendly_uuid="6",
+            booking=Booking.objects.create(
+                email="aa@localhost",
+                event_type_id="2",
+                spot_start="2019-01-01 10:00:00-0400",
+                spot_end="2019-01-01 10:10:00-0400",
+                approval_status=Booking.APPROVAL_STATUS_NEW,
+            ),
+        )
+
+        changed = ag.execute_approval([bc2.booking], [bc3.booking], fake=True)
+
+        bc2.refresh_from_db()
+        bc3.refresh_from_db()
+        self.assertEqual(bc2.booking.approval_status, Booking.APPROVAL_STATUS_NEW)
+        self.assertEqual(bc3.booking.approval_status, Booking.APPROVAL_STATUS_NEW)
+        self.assertEqual(bc2.approval_group, None)
+        self.assertEqual(bc3.approval_group, None)
+        self.assertEqual(changed[0].approval_status, Booking.APPROVAL_STATUS_APPROVED)
+        self.assertEqual(changed[1].approval_status, Booking.APPROVAL_STATUS_DECLINED)
+        self.assertEqual(changed[0].calendly_data.approval_group, ag)
+        self.assertEqual(changed[1].calendly_data.approval_group, ag)
+
+    def test_execute_approval_meta(self):
+        "approval_group, approval_status (approved/declined) and log_action, returns changed"
+
+
+    def test_execute_approval_protected(self):
+        "protected is not touched, returns correct result"
+
+    def test_bc_run_approval_noemail(self):
+        "noemail should be untouched"
+
+    def test_bc_run_approval_group(self):
+        pass
+
+    def test_bc_run_approval_nogroup_decline(self):
+        "APPROVAL_TYPE_DECLINE"
+
+    def test_bc_run_approval_nogroup_manual(self):
+        "APPROVAL_TYPE_MANUAL"
+
+
 class HookAdminTests(TestCase):
     def setUp(self):
         self.client = Client()
